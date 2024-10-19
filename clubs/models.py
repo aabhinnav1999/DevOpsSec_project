fde8b9e90df534c8efce1561edcf0310ea9caac8
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2024-10-19 12:34:53 +0100

    added user registration

--- clubs/models.py
+++ clubs/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Clubs(models.Model):
-    # host=
+    host=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     name=models.CharField(max_length=50)
     description=models.TextField(null=True,blank=True)
     # members=
--- clubs/views.py
+++ clubs/views.py
@@ -1,7 +1,59 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
 from .models import Clubs
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def loginpage(request):
+    page='login'
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method=='POST':
+        username=request.POST['username'].lower()
+        password=request.POST['password']
+
+        try:
+            user=User.objects.get(username=username)
+
+
+        except:
+            messages.error(request,'user does not exist')
+
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'username or password does not exist')
+
+
+    context={'page':page}
+    return render(request,'clubs/register.html',context)
+
+def logoutuser(request):
+    logout(request)
+    return redirect('home')
+
+def registeruser(request):
+    # page='register'
+    form=UserCreationForm()
+
+    if request.method=="POST":
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.username=user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'an error occured during registration')
+    return render(request,'clubs/register.html',{'form':form})
 
 def home(request):
     clubs=Clubs.objects.all()
@@ -13,6 +65,7 @@ def club(request,pk):
     context={'clubs':clubs}
     return render(request,'clubs/club.html',context)
 
+@login_required(login_url='login')
 def createclub(request):
     if request.method=="POST":
         cname=request.POST['cname']
@@ -22,12 +75,32 @@ def createclub(request):
         return redirect('home')
     return render(request,'clubs/createclub.html')
 
+@login_required(login_url='login')
 def updateclub(request,pk):
     club=Clubs.objects.get(id=pk)
+    res=club.description
+    # print(res)
     context={'club':club}
+    
+    if request.user != club.host:
+        return HttpResponse("you are not allowed here!")
     if request.method=="POST":
         cname=request.POST['cname']
         description=request.POST['description']
+        # print(type(description),len(description))
+        if len(description)==0:
+            description=res
         Clubs.objects.filter(id=pk).update(name=cname,description=description)
         return redirect('home')
-    return render(request,'clubs/updateroom.html',context)
+    return render(request,'clubs/updateclub.html',context)
+
+@login_required(login_url='login')
+def deleteclub(request,pk):
+    club=Clubs.objects.get(id=pk)
+    if request.user != club.host:
+        return HttpResponse("you are not allowed here!")
+    if request.method=='POST':
+        club.delete()
+        return redirect('home')
+    return render(request, 'clubs/deleteclub.html',{'obj':club})
+
