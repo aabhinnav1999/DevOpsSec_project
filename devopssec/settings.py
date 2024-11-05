626d694ef0ea282997865b2a69a18442b47d396f
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2024-11-05 19:54:10 +0000

    installed whitenoise

--- devopssec/settings.py
+++ devopssec/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,9 +125,7 @@ STATIC_URL = 'static/'
 #     BASE_DIR/'static'
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
-
-# STATIC_ROOT = 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
