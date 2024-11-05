b1dc5cab3a857dbcd8972fbdb0b6aa56e501b687
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2024-11-05 19:20:15 +0000

    added python-decouple

--- devopssec/settings.py
+++ devopssec/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config,Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v%btv)$*u&gpij*%#a(abhowvj43_)ng@5@n==-1@4l*4k^new'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',default=False,cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS',cast=Csv())
 
 
 # Application definition
