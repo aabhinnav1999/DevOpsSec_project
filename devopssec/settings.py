faf07c2cf644faeeb2eaf6b17451b3fc19ef8517
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2024-11-05 19:35:41 +0000

    added static root

--- devopssec/settings.py
+++ devopssec/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config,Csv
 
@@ -119,9 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR/'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR/'static'
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
 # STATIC_ROOT = 
 
