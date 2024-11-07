95a2e381dfff3b67b0765464298608c62b10983a
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2024-11-07 23:45:20 +0000

    updated gunicorn.sh

--- gunicorn/gunicorn.sh
+++ gunicorn/gunicorn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo cp gunicorn.socket /etc/systemd/system/gunicorn.socket
-sudo cp gunicorn.service /etc/systemd/system/gunicorn.service
+sudo cp /home/ubuntu/cicd/workspace/django_pipeline/gunicorn.socket /etc/systemd/system/gunicorn.socket
+sudo cp /home/ubuntu/cicd/workspace/django_pipeline/gunicorn.service /etc/systemd/system/gunicorn.service
 sudo systemctl start gunicorn.socket
 sudo systemctl enable gunicorn.socket
 sudo systemctl daemon-reload
