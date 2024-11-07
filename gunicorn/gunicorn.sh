45d1e000232cb8d43e8cac1c587b3fbe4c18ef8e
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2024-11-07 23:39:49 +0000

    testing gunicorn.sh

--- gunicorn/gunicorn.sh
+++ gunicorn/gunicorn.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-sudo cp gunicorn.socket /etc/systemd/system/gunicorn.socket
-sudo cp gunicorn.service /etc/systemd/system/gunicorn.service
-sudo systemctl start gunicorn.socket
-sudo systemctl enable gunicorn.socket
-sudo systemctl daemon-reload
-sudo systemctl restart gunicorn
+# sudo cp gunicorn.socket /etc/systemd/system/gunicorn.socket
+# sudo cp gunicorn.service /etc/systemd/system/gunicorn.service
+# sudo systemctl start gunicorn.socket
+# sudo systemctl enable gunicorn.socket
+# sudo systemctl daemon-reload
+# sudo systemctl restart gunicorn
+
+pwd
+ls
