c78359e711343f82720228abc4cb3e7ddff51a89
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2024-11-10 19:09:43 +0000

    updated settings.py

--- devopssec/settings.py
+++ devopssec/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-v%btv)$*u&gpij*%#a(abhowvj43_)ng@5@n==-1@4l*4k^new
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False,cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS',cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split()
 
 
 # Application definition
--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -2,5 +2,7 @@
 sudo apt update
 sudo apt install nginx -y
 sudo cp devopssec /etc/nginx/sites-available/devopssec
+sudo rm -rf /etc/nginx/sites-available/default
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/devopssec /etc/nginx/sites-enabled
 sudo systemctl restart nginx
