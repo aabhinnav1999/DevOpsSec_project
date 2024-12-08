d52be46dfc77af05c133d03a59f1b9a3ce39003d
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2024-12-08 18:01:03 +0000

    updated gunicorn.sh and nginx.sh

--- gunicorn_folder/gunicorn.sh
+++ gunicorn_folder/gunicorn.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-sudo cp gunicorn.socket /etc/systemd/system/gunicorn.socket
-sudo cp gunicorn.service /etc/systemd/system/gunicorn.service
+if [ -f "/etc/systemd/system/gunicorn.socket" ]; then
+    echo "file exists"
+else
+    sudo cp gunicorn.socket /etc/systemd/system/gunicorn.socket
+fi
+if [ -f "/etc/systemd/system/gunicorn.service" ]; then
+    echo "file exists"
+else
+    sudo cp gunicorn.service /etc/systemd/system/gunicorn.service
+fi
 sudo systemctl start gunicorn.socket
 sudo systemctl enable gunicorn.socket
 sudo systemctl daemon-reload
--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 sudo apt update
 sudo apt install nginx -y
-sudo cp devopssec /etc/nginx/sites-available/devopssec
-sudo rm -rf /etc/nginx/sites-available/default
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -s /etc/nginx/sites-available/devopssec /etc/nginx/sites-enabled
+if [ -f "/etc/nginx/sites-available/devopssec" ]; then
+    echo "file exists"
+else
+    sudo cp devopssec /etc/nginx/sites-available/devopssec
+fi
+if [ -f "/etc/nginx/sites-available/default" ]; then
+    sudo rm -rf /etc/nginx/sites-available/default
+else
+    echo "file already deleted"
+fi
+if [ -f "/etc/nginx/sites-enabled/default" ]; then
+    sudo rm -rf /etc/nginx/sites-enabled/default
+else
+    echo "file already deleted"
+fi
+if [ -L "/etc/nginx/sites-enabled/devopssec" ] && [ "$(readlink -f /etc/nginx/sites-enabled/devopssec)" == "/etc/nginx/sites-available/devopssec" ]; then
+    echo "The site is correctly linked."
+else
+    echo "The site is not linked. Linking now..."
+    sudo ln -sf /etc/nginx/sites-available/devopssec /etc/nginx/sites-enabled/devopssec
+    echo "Link created."
+fi
 sudo systemctl restart nginx
