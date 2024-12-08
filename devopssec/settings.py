369f532ca9e7be010ddc1c884531d91dbc73dafb
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2024-12-08 19:58:17 +0000

    updated settings.py

--- devopssec/settings.py
+++ devopssec/settings.py
@@ -79,13 +79,6 @@ WSGI_APPLICATION = 'devopssec.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
